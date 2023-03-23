d8751e870d84ea80d5c840aff6311ccd4d85a922
string <799034552@qq.com>
2023-03-23 17:53:27 +0800

    修改撞墙

--- robot.cpp
+++ robot.cpp
@@ -1,247 +1,80 @@
-// #include "robot.h"
-// #include "controler.h"
-// #include <iostream>
-// #include <cmath>
-
-// using namespace std;
-
-// /// @brief 计算机器人离哪面墙最近
-// /// @param pos 机器人坐标
-// /// @param face 机器人朝向
-// /// @return 机器人距离墙的距离；机器人是否正在接近墙
-// std::pair<double, double> robot_wall_relation(std::pair<double, double> pos, double face)
-// {
-//     double distance_to_wall = MAX_NUMBER;
-//     double angle;
-//     // 右边的墙
-//     if (50 - pos.first < distance_to_wall)
-//     {
-//         distance_to_wall = 50 - pos.first;
-//         angle = face;
-//     }
-//     // 左边的墙
-//     if (pos.first < distance_to_wall)
-//     {
-//         distance_to_wall = pos.first;
-//         angle = face - PI;
-//     }
-//     // 上边的墙
-//     if (50 - pos.second < distance_to_wall)
-//     {
-//         distance_to_wall = 50 - pos.second;
-//         angle = face - PI / 2;
-//     }
-//     // 下边的墙
-//     if (pos.second < distance_to_wall)
-//     {
-//         distance_to_wall = pos.second;
-//         angle = face + PI / 2;
-//     }
-//     return std::pair<double, double>(distance_to_wall, angle);
-// }
-
-// /// @brief 返回一个点离墙面的最近距离
-// /// @param pos
-// /// @return
-// double distance_pos_wall(std::pair<double, double> pos)
-// {
-//     double distance_to_wall = MAX_NUMBER;
-//     // 右边的墙
-//     if (50 - pos.first < distance_to_wall)
-//     {
-//         distance_to_wall = 50 - pos.first;
-//     }
-//     // 左边的墙
-//     if (pos.first < distance_to_wall)
-//     {
-//         distance_to_wall = pos.first;
-//     }
-//     // 上边的墙
-//     if (50 - pos.second < distance_to_wall)
-//     {
-//         distance_to_wall = 50 - pos.second;
-//     }
-//     // 下边的墙
-//     if (pos.second < distance_to_wall)
-//     {
-//         distance_to_wall = pos.second;
-//     }
-//     return distance_to_wall;
-// }
-
-// /// @brief 计算向量模长
-// /// @param vec
-// /// @return
-// double length(std::pair<double, double> vec)
-// {
-//     return sqrt(vec.first * vec.first + vec.second + vec.second);
-// }
-
-// std::pair<double, double> Robot::Robot_controle(double distance, double angle)
-// {
-//     static double kp = 20;                           //  转弯p参数
-//     static double kd = 0.01;                         //  转弯d参数
-//     static double k1 = 1.8;                          //  前进速度参数，用转弯半径控制速度
-//     static double k2 = 2;                            //  前进速度参数，用距离工作台的距离控制速度
-//     static double k3 = 0.2;                          //  前进速度参数,用离墙的距离控制速度
-//     static double rotate_radius = 2.18;              // 最大转弯半径
-//     static double rotate_angle_threshold = 0.4;      // 角度大于此阈值，考虑减速
-//     static double wall_angle_threshold = 3 * PI / 5; // 机器人与墙壁的角度小于此阈值，考虑减速
-
-//     double forward_speed = 0;
-//     double rotate_speed = 0;
-
-//     double angle_diff = angle - this->face;
-//     if (angle_diff > PI)
-//         angle_diff -= 2 * PI;
-//     else if (angle_diff < -PI)
-//         angle_diff += 2 * PI;
-
-//     // 负数表示顺时针旋转
-//     rotate_speed = kp * angle_diff - kd * this->rotate_speed;
-
-//     // 计算转弯的圆心坐标
-//     std::pair<double, double> po((-this->rotate_speed) * (this->linear_speed.second),
-//                                  (this->rotate_speed) * (this->linear_speed.first));       // 机器人指向圆心的半径向量，叉乘运算
-//     std::pair<double, double> o(this->pos.first + po.first, this->pos.second + po.second); // 圆心坐标
-//     double r = length(po);                                                                 // 半径长度
-//     double dis = distance_pos_wall(o);                                                     // 圆心与墙面距离
-
-//     std::pair<double, double> robot_wall_re = robot_wall_relation(this->pos, this->face);   // 计算离墙壁的情况
-//     bool a = distance < 2 * rotate_radius && abs(angle_diff) > rotate_angle_threshold;      // 防转圈
-//     bool b = robot_wall_re.first < 0.8 && abs(robot_wall_re.second) < wall_angle_threshold; // 防撞墙
-
-//     bool test = false;
-//     if ((abs(robot_wall_re.second) < wall_angle_threshold) && (dis <= r + 2.5))
-//     {
-//         forward_speed = 0.3 * dis;
-//         test = true;
-//     }
-//     else if (a)
-//     {
-//         forward_speed = -k1 * abs(angle_diff) + k2 * distance;
-//         forward_speed = max(3.0, forward_speed);
-//     }
-//     else
-//     {
-//         forward_speed = 6.0;
-//     }
-
-//     // 加上撞墙逻辑
-//     // auto & wb = wb_list[this->forward_id];
-//     // auto min_dis = min(min(wb.pos.first, 50 - wb.pos.first), min(wb.pos.second, 50 - wb.pos.second)); // 目标离墙距离
-//     // auto target_dis = cal_distance(this->pos, wb.pos);
-//     // if (min_dis < 3 && target_dis < 1 && cal_distance(this->linear_speed, {0,0}) > 4) {
-//     //     forward_speed = 0;
-//     // }
-
-    
-
-
-//     // if (this->id == 1 && frame_id > 620 && frame_id < 650)
-//     // {
-//     //     cerr << frame_id << " " << test << endl;
-//     //     cerr << "set forward speed: " << forward_speed << " set rotate speed: " << rotate_speed << endl;
-//     //     cerr << "actual forward speed: " << length(this->linear_speed) << " actual rotate speed: " << this->rotate_speed << endl;
-//     //     cerr << "distance o to wall:" << dis << " radius:" << r;
-//     //     cerr << this->linear_speed.first << ", " << this->linear_speed.second;
-//     //     cerr << distance << " " << angle_diff << endl;
-//     //     cerr << robot_wall_re.first << " " << robot_wall_re.second << endl;
-//     //     cerr << this->face << " " << this->pos.first << " " << this->pos.second << endl;
-//     //     cerr << "----------------------------------->" << endl;
-//     // }
-
-//     std::pair<double, double> result(forward_speed, rotate_speed);
-//     return result;
-// }
-
 #include "robot.h"
+#include "controler.h"
 #include <iostream>
 #include <cmath>
-#define PI 3.14159265359
-#define MAX_NUM 5000000
-int k = 0;
-
 using namespace std;
-
-/// @brief 计算机器人离哪面墙最近
-/// @param pos 机器人坐标
-/// @param face 机器人朝向
-/// @return 机器人距离墙的距离；机器人是否正在接近墙
-std::pair<double, double> robot_wall_relation(std::pair<double, double> pos, double face)
-{
-    double distance_to_wall = MAX_NUM;
-    double angle;
-    // 右边的墙
-    if (50 - pos.first < distance_to_wall)
-    {
-        distance_to_wall = 50 - pos.first;
-        angle = face;
-    }
-    // 左边的墙
-    if (pos.first < distance_to_wall)
-    {
-        distance_to_wall = pos.first;
-        angle = face - PI;
-    }
-    // 上边的墙
-    if (50 - pos.second < distance_to_wall)
-    {
-        distance_to_wall = 50 - pos.second;
-        angle = face - PI / 2;
-    }
-    // 下边的墙
-    if (pos.second < distance_to_wall)
-    {
-        distance_to_wall = pos.second;
-        angle = face + PI / 2;
-    }
-    return std::pair<double, double>(distance_to_wall, angle);
-}
 
 std::pair<double, double> Robot::Robot_controle(double distance, double angle)
 {
-    static double kp = 20;                       //  转弯p参数
-    static double kd = 0.01;                     //  转弯d参数
-    static double k1 = 1.8;                      //  前进速度参数，用转弯半径控制速度
-    static double k2 = 2;                        //  前进速度参数，用距离工作台的距离控制速度
-    static double k3 = 1;                        //  前进速度参数,用离墙的距离控制速度
-    static double rotate_radius = 2.18;          // 最大转弯半径
-    static double rotate_angle_threshold = 0.4;  // 角度大于此阈值，考虑减速
-    static double wall_angle_threshold = PI / 2; // 机器人与墙壁的角度小于此阈值，考虑减速
+    if (this->forward_id == -1) return {0, 0};
+    double forward_speed = 0, rotate_speed = 0;
+    auto &wb = wb_list[this->forward_id];
 
-    double forward_speed = 0;
-    double rotate_speed = 0;
-
-    double angle_diff = angle - this->face;
-    if (angle_diff > PI)
-        angle_diff -= 2 * PI;
-    else if (angle_diff < -PI)
-        angle_diff += 2 * PI;
-
-    // 负数表示顺时针旋转
-    rotate_speed = kp * angle_diff - kd * this->rotate_speed;
-
-    // 计算离墙壁的情况
-    std::pair<double, double> robot_wall_re = robot_wall_relation(this->pos, this->face);
-
-    bool a = distance < 2 * rotate_radius && abs(angle_diff) > rotate_angle_threshold;                      // 防转圈
-    bool b = robot_wall_re.first < 0.3 * rotate_radius && abs(robot_wall_re.second) < wall_angle_threshold; // 防撞墙
-    if (b)
-    {
-        forward_speed = k3 * robot_wall_re.first;
+    // 速度控制参数, 位置式pid
+    static double kp_speed = 6;
+    static double kp_speed_wall = 4;
+    static double ki_speed = 1;
+    static double kd_speed = 0;
+    // 转动控制参数, 位置式pid
+    static double kp_rotate = 30;
+    static double ki_rotate = 1;
+    static double kd_rotate = 0;
+    // 如果目标不同了，清除累计误差等参数
+    if (this->last_forward_id != this->forward_id) {
+        this->last_dis_diff = -1;
+        this->last_rotate_diff = -1;
     }
-    else if (a)
-    {
-        forward_speed = -k1 * abs(angle_diff) + k2 * distance;
-        // forward_speed = -k1 * abs(angle_diff) + k2 * distance;
-        forward_speed = max(3.0, forward_speed);
+    // 计算距离误差与角度误差
+    auto dis_diff = cal_distance(this->pos, wb.pos);
+    if (this->last_dis_diff == -1) this->last_dis_diff = dis_diff;
+    
+    auto target_rotate = cal_angle(this->pos, wb.pos);
+    auto rotate_diff = cal_rotate_diff(target_rotate, this->face);
+    if (this->last_rotate_diff == -1) this->last_rotate_diff = rotate_diff;
+
+    double speed_p = kp_speed;
+    if (cal_wall_dis(wb.pos) < 4) {
+        speed_p = kp_speed_wall;
     }
-    else
-    {
-        forward_speed = 6.0;
+    if (abs(rotate_diff) - abs(last_rotate_diff) > -0.2 && rotate_diff > PI/180*30) { // 如果角度变小误差,并且距离
+        forward_speed = 1;
+    } else if (abs(rotate_diff) < PI/4) {
+        forward_speed = speed_p * dis_diff + kd_speed * (dis_diff - last_dis_diff);
+    }
+    else if (abs(rotate_diff) < PI/2) {
+        if (cal_wall_dis(pos) < 5) {
+            forward_speed = 1;
+        } else {
+            forward_speed = speed_p * dis_diff + kd_speed * (dis_diff - last_dis_diff);
+        }
+    } else {
+        forward_speed = 1;
     }
 
-    std::pair<double, double> result(forward_speed, rotate_speed);
-    return result;
+    // 转动控制
+    rotate_speed = kp_rotate * rotate_diff + kd_rotate * (rotate_diff - last_rotate_diff);
+
+    return {forward_speed, rotate_speed};
+}
+
+double cal_rotate_diff(double target, double face) {
+    auto dif = target - face;
+    if (target >= face) {
+        if (dif <= PI)
+            return dif;
+        else
+            return -(2*PI - dif);
+    }
+    else {
+        dif = -dif;
+        if (dif <= PI)
+            return -dif;
+        else
+            return 2*PI - dif;
+    }
+    return 0;
+}
+
+double cal_wall_dis(pair<double, double> pos) {
+    return min(min(50 - pos.first, pos.second), min(50 - pos.first, pos.second));
 }
--- robot.h
+++ robot.h
@@ -3,6 +3,8 @@
 #include <utility>
 
 enum Action {None, buy, sell, wait};
+
+
 class Robot {
     public:
     int id;
@@ -17,7 +19,16 @@ class Robot {
 
     int forward_id = -1; //目标工作台，-1表示没有
     int action = None; 
+    bool target_can_change = true;
+    double dis_err = -1;
+    double last_dis_diff = -1;
+    double last_rotate_diff = -1;
+    int last_forward_id = -1;
     Robot() {};
     std::pair<double,double> Robot_controle(double distance, double angle); //给定距离目标的距离和角度，输出前进速度和旋转速度
 };
+
+double cal_rotate_diff(double target, double face);
+double cal_wall_dis(std::pair<double, double> pos);
+
 #endif
